e6e865b63b3b869677df62957eef5afdc410d5ac
Matthew Ewer <Ewer.Matthew@gmail.com>
2014-10-09 23:27:01 -0700

    New repo; program to generate a QR code, for use in quickly copying stuff to a device.

---
+++ src/qrcodetest/QRCodeTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qrcodetest;
+
+/**
+ *
+ * @author MEwer
+ */
+public class QRCodeTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new QRCodeFrame().setVisible(true);
+    }
+}
